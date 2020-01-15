17ebbf037eaeb0b45d0c244f03fe74a1f1fdfc63
Abhay Kumar <Abhay.Kumar.con@derivco.com>
2020-01-15 14:39:56 +0530

    Added CORS.

--- WebApi/Controllers/EmployeeController.cs
+++ WebApi/Controllers/EmployeeController.cs
@@ -5,9 +5,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApi.Models;
+using System.Web.Http.Cors;
 
 namespace WebApi.Controllers
 {
+    [EnableCorsAttribute("*", "*", "*")]
     public class EmployeeController : ApiController
     {
         public List<Employee> Get()
@@ -37,6 +39,18 @@ namespace WebApi.Controllers
                 message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
                 return message;
             }
+        }
+     
+        [Route("api/Employee/getSalary")]
+        [HttpGet]
+        public IEnumerable<string> SalaryData()
+        {
+            using (EmployeeEntities DBContext = new EmployeeEntities())
+            {
+                IEnumerable<string> salaryList = DBContext.Employees.Select(e => e.Salary.ToString()).ToList();
+                return salaryList;
+            }
+
         }
 
 
