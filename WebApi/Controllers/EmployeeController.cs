b7ea5aaa07bac46e4fb8c0c1f75bac97f70af232
Abhay Kumar <Abhay.Kumar.con@derivco.com>
2020-01-15 13:37:50 +0530

    Added Employee DB Model and controller.

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public List<Employee> Get()
+        {
+            using (EmployeeEntities DBContext = new EmployeeEntities())
+            {
+                return DBContext.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeEntities DBContext = new EmployeeEntities())
+            {
+                return DBContext.Employees.FirstOrDefault(e=>e.ID==id);
+            }
+        }
+
+
+        public HttpResponseMessage Post(Employee employee)
+        {
+            using (EmployeeEntities DBContext = new EmployeeEntities())
+            {
+                DBContext.Employees.Add(employee);
+                DBContext.SaveChanges();
+                var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                return message;
+            }
+        }
+
+
+    }
+}
